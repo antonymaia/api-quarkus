ad8fc4bf604333778105e8bee5ca12964e92c37d
Antony Maia <antonymaiati@gmail.com>
2021-11-09 19:44:51 -0300

    Atualizado

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Primeira aplicação Quarkus";
+        return "Primeira aplicação Quarkus Atualizada";
     }
 }
